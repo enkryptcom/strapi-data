d9fde9c2ed6bbba773f795d12888f826d7a9070b
olgakup <olga.kupchevskaya.812@my.csun.edu>
2023-12-11 13:34:11 -0800

    feat: add ethvm tokens page

---
+++ src/api/ethvm-token-page/routes/ethvm-token-page.ts
@@ -0,0 +1,7 @@
+/**
+ * ethvm-token-page router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::ethvm-token-page.ethvm-token-page');
---
+++ src/api/ethvm-token-page/services/ethvm-token-page.ts
@@ -0,0 +1,7 @@
+/**
+ * ethvm-token-page service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::ethvm-token-page.ethvm-token-page');
